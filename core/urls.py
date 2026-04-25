99a0dffd80ef7af8d5e85c1eceb99bc9d38cb0b5
nacht1234 <ralphperoch1789@gmail.com>
2026-04-25 17:57:25 +0800

    add viewset, config urls

--- core/urls.py
+++ core/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('api/v1/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     
-    path('api/v1/', include('records.urls')),
+    path('api/v1/', include('student_records.urls')),
 ]
--- student_records/views.py
+++ student_records/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import StudentRecord
+from .serializers import StudentRecordSerializer
+from .permissions import IsAdminOrFaculty
 
-# Create your views here.
+class StudentRecordViewSet(ModelViewSet):
+    queryset = StudentRecord.objects.all()
+    serializer_class = StudentRecordSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'destroy']:
+            permission_classes = [IsAdminUser]
+        elif self.action in ['update', 'partial_update']:
+            permission_classes = [IsAdminOrFaculty]
+        else:
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.groups.filter(name="Student").exists():
+            return StudentRecord.objects.filter(owner=user)
+
+        return StudentRecord.objects.all()
